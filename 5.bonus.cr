ade6cd0d82802ed8e4270611321b9be2ecf9284d
David Schovanec <david@schovi.cz>
2019-12-15 15:58:00 +0100

    5

---
+++ 5.bonus.cr
@@ -0,0 +1,11 @@
+require "file"
+require "./cpu"
+
+run(File.read("5.input"), [5])
+
+# test = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
+# puts "should be 0:"
+# run(test, [0])
+
+# puts "should be 1:"
+# run(test, [1])
---
+++ cpu.cr
@@ -0,0 +1,94 @@
+def read_value(memory, cursor, parameter, modes)
+  position = memory[cursor + parameter]
+
+  if modes[parameter] == '0'
+    memory[position]
+  else
+    position
+  end
+end
+
+def write_value(memory, cursor, parameter, value)
+  memory[memory[cursor + parameter]] = value
+end
+
+def run(memory, prompt)
+  puts "program starts:"
+
+  memory = memory.split(',').map(&.to_i)
+  cursor = 0
+
+  while
+    instruction = memory[cursor]
+    opcode      = instruction % 100
+    parsed      = instruction.to_s.chars
+    modes       = {
+      nil,
+      parsed[-3]? || '0',
+      parsed[-4]? || '0',
+      parsed[-5]? || '0'
+    }
+
+    case opcode
+    when 1
+      value = read_value(memory, cursor, 1, modes) + read_value(memory, cursor, 2, modes)
+      write_value(memory, cursor, 3, value)
+
+      cursor += 4
+    when 2
+
+      value = read_value(memory, cursor, 1, modes) * read_value(memory, cursor, 2, modes)
+      write_value(memory, cursor, 3, value)
+
+      cursor += 4
+    when 3
+      # Lets fake user input 🤷🏻‍♂️
+      value = prompt.shift
+
+      unless value
+        raise "missing prompt value"
+      end
+
+      write_value(memory, cursor, 1, value)
+
+      cursor += 2
+    when 4
+      # Print output
+      puts read_value(memory, cursor, 1, modes)
+
+      cursor += 2
+    when 5 # jump-if-true
+      if read_value(memory, cursor, 1, modes) != 0
+        cursor = read_value(memory, cursor, 2, modes)
+      else
+        cursor += 3
+      end
+    when 6 # jump-if-false
+      if read_value(memory, cursor, 1, modes) == 0
+        cursor = read_value(memory, cursor, 2, modes)
+      else
+        cursor += 3
+      end
+    when 7 # less than
+      if read_value(memory, cursor, 1, modes) < read_value(memory, cursor, 2, modes)
+        write_value(memory, cursor, 3, 1)
+      else
+        write_value(memory, cursor, 3, 0)
+      end
+
+      cursor += 4
+    when 8 # equals
+      if read_value(memory, cursor, 1, modes) == read_value(memory, cursor, 2, modes)
+        write_value(memory, cursor, 3, 1)
+      else
+        write_value(memory, cursor, 3, 0)
+      end
+
+      cursor += 4
+    when 99
+      break
+    else
+      raise "unknown opcode #{opcode} at #{cursor} instruction #{instruction}"
+    end
+  end
+end
