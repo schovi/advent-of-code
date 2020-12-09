015290bb3114ebeb4f1ef79367fb13473ae12fe0
David Schovanec <david@schovi.cz>
2020-12-09 08:46:29 +0100

    2020 Day 9 bonus

--- 2020/D9.hs
+++ 2020/D9.hs
@@ -11,19 +11,22 @@ main = do
   let result = solution 25 $ map fromInteger $ readNumbers "inputs/9.input"
   print result
 
-  -- print "Bonus Test:"
-  -- let result = bonusSolution $ readLines "inputs/8.test.input"
-  -- print result
+  print "Bonus Test:"
+  let result = bonusSolution 5 $ map fromInteger $ readNumbers "inputs/9.test.input"
+  print result
 
-  -- print "Bonus:"
-  -- let result = bonusSolution $ readLines "inputs/8.input"
-  -- print result
+  print "Bonus:"
+  let result = bonusSolution 25 $ map fromInteger $ readNumbers "inputs/9.input"
+  print result
 
 solution :: Int -> [Int] -> Int
-solution preambleSize numbers = case find findNonCountable combinations of
-                                  Just (value,_) -> value
-                                  _          -> error "boom"
-  where combinations              = sliceNumbers preambleSize numbers
+solution = findContiguousNumber
+
+findContiguousNumber :: Int -> [Int] -> Int
+findContiguousNumber preambleSize numbers =
+  let combinations = sliceNumbers preambleSize numbers
+      found        = find findNonCountable combinations
+  in case found  of Just (value,_) -> value; _ -> error "boom"
 
 sliceNumbers :: Int -> [Int] -> [(Int, [Int])]
 sliceNumbers preambleSize numbers@(_:next)
@@ -35,3 +38,20 @@ sliceNumbers preambleSize numbers@(_:next)
 findNonCountable :: (Int, [Int]) -> Bool
 findNonCountable (theNumber, numbers) = null combinations
   where combinations = [ (x,y) | x <- numbers, y <- numbers, x + y == theNumber ]
+
+-- bonus
+
+bonusSolution :: Int -> [Int] -> Int
+bonusSolution preambleSize numbers =
+  let contiguousNumber = findContiguousNumber preambleSize numbers
+      sequences        = followingSequences numbers
+      found            = find finder sequences
+      finder sequence  = sum sequence == contiguousNumber
+  in case found of Just sequence -> minimum sequence + maximum sequence; _ -> error "boom"
+
+followingSequences :: [a] -> [[a]]
+followingSequences numbers@(frst:scnd:rest) = currentSequences ++ followingSequences (tail numbers)
+  where beginning        = [[frst, scnd]]
+        currentSequences = foldl folder beginning rest
+        folder seq item  = seq ++ [last seq ++ [item]]
+followingSequences _ = []
